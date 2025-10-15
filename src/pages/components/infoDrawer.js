55b3eee8c6ce1fc03917debe6a157ccc309a6248
Anudeep Roy <48355379+AnudeepRoy@users.noreply.github.com>
2025-10-15 17:35:40 +0530

    Update favicon and improve info drawer logic
    Replaced the default favicon with pokeball.ico and updated the reference in index.js. Improved conditional rendering in InfoDrawer to show the skeleton only when pokemonInfo is null. Updated plan.txt to reflect completed tasks.

--- src/pages/components/infoDrawer.js
+++ src/pages/components/infoDrawer.js
@@ -22,24 +22,25 @@ export default function InfoDrawer({open, onClose, pokemon}) {
 
 
     return (
-        pokemonInfo === null ?
-        <Skeleton variant="rounded" width={210} height={60} />
-        :
         <Drawer 
             anchor="left"
             open={open}
             onClose={onClose}
         >
-            <div className="drawer-container" style={{width: 250, padding: 20}}>
-                <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonInfo?.id}.png`} />
-                <h2>{pokemonInfo?.name}</h2>
-                <p>ID: {pokemonInfo?.id}</p>
-                <p>Height: {pokemonInfo?.height}</p>
-                <p>Weight: {pokemonInfo?.weight}</p>
-                <p>Base Experience: {pokemonInfo?.base_experience}</p>
-                <p>Types: {pokemonInfo?.types.map(typeInfo => typeInfo.type.name).join(", ")}</p>
-                <p>Abilities: {pokemonInfo?.abilities.map(abilityInfo => abilityInfo.ability.name).join(", ")}</p>
-            </div>
+            {pokemonInfo === null ?
+                <Skeleton variant="rounded" width={210} height={60} />
+                :
+                <div className="drawer-container" style={{width: 250, padding: 20}}>
+                    <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonInfo?.id}.png`} />
+                    <h2>{pokemonInfo?.name}</h2>
+                    <p>ID: {pokemonInfo?.id}</p>
+                    <p>Height: {pokemonInfo?.height}</p>
+                    <p>Weight: {pokemonInfo?.weight}</p>
+                    <p>Base Experience: {pokemonInfo?.base_experience}</p>
+                    <p>Types: {pokemonInfo?.types.map(typeInfo => typeInfo.type.name).join(", ")}</p>
+                    <p>Abilities: {pokemonInfo?.abilities.map(abilityInfo => abilityInfo.ability.name).join(", ")}</p>
+                </div>
+            }
         </Drawer>
 
     );
